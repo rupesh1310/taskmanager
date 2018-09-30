c1918a06e2045a778f0457bd47dde3d2389da993
A.Rupesh <a.rupesh66@gmail.com>
2018-09-30 12:53:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300
+  });
 mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
