f05ddbd194e781105fc7be903177f8046cd29142
A.Rupesh <a.rupesh66@gmail.com>
2018-09-30 13:19:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let Tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -15,5 +16,5 @@ app.on('ready', () => {
 mainWindow.loadURL(`file://${__dirname}/src/index.html`);
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
- new Tray(iconPath);
+tray = new Tray(iconPath);
 });
