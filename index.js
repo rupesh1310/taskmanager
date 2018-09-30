335271b750f52fbf14bd33deca5a7b2dcdb8f7c9
A.Rupesh <a.rupesh66@gmail.com>
2018-09-30 12:41:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready'. () => {
+  mainWindow = new BrowserWindow({});
+mainWindow.loadURL(`file://${__dirname}/`);
+});
