41db8cbe49ab7eb7a4d0211e82ffb75bb7207187
A.Rupesh <a.rupesh66@gmail.com>
2018-09-30 13:06:15 +0530

    tray icon created

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.on('ready', () => {
   });
 mainWindow.loadURL(`file://${__dirname}/src/index.html`);
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   new Tray();
   
 });
